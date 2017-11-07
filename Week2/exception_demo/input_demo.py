08b1d95ae65c819c0cda09bf58945cf33a271df9
luke <ltaylor255@gmail.com>
2017-11-07 14:12:45 -0600

    Added interactive command system and more exceptions

--- Week2/exception_demo/input_demo.py
+++ Week2/exception_demo/input_demo.py
@@ -1,9 +1,18 @@
+import Week2.mymath.shapes as shapes
+
 def run_demo():
     try:
+        print("Enter two floats.  Other values will generate exceptions.")
         i = input()
+        width, height = map(float, i.split())
     except EOFError:
         print("End of file was entered.")
     except KeyboardInterrupt:
         print("Kill signal was thrown.")
+    except ValueError:
+        print("Enter exactly two floats")
+    else:
+        s = shapes.Rectangle(width, height)
+        print("Rectangle with area = " + str(s.area()) + " and perimeter = " + str(s.perimeter()))
     finally:
-        print("You entered: " + i)
+        print("Exiting...")
--- Week2/main.py
+++ Week2/main.py
@@ -1,16 +1,22 @@
 import Week2.mymath
 import Week2.exception_demo
+import sys
+
 if __name__ == "__main__":
-    print("Enter a command:")
-    command = input()
-    if("sin" in command):
-        while(True):
-            val = input()
-            if(val == "exit"):
-                exit(0)
-            print(Week2.mymath.math.sin(float(val)))
-    elif("sq" in command):
-        sq = Week2.mymath.shapes.Square(3)
-        print(sq.area())
-    else:
-        Week2.exception_demo.input_demo.run_demo()
+    print("Enter a command.  Type help for usage")
+    while(True):
+        sys.stdout.write("> ")
+        command = input()
+        if("sin" in command):
+            val = float(command.split()[1])
+            print(Week2.mymath.math.sin(val))
+        elif("sq" in command):
+            val = float(command.split()[1])
+            sq = Week2.mymath.shapes.Square(val)
+            print(sq.area())
+        elif("help" in command):
+            print("Type sin for a demo of sine, square for a demo of a square's area, except for a demo of exceptions, or exit to quit.")
+        elif("exit" in command):
+            exit(0)
+        else:
+            Week2.exception_demo.input_demo.run_demo()
